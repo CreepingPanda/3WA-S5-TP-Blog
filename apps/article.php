c641b8f87130b33f252c4d5b111045140439a856
CreepingPanda <dhrask@voila.fr>
2015-11-16 15:43:48 +0100

    Update artlist & article

--- apps/article.php
+++ apps/article.php
@@ -1,3 +1,9 @@
 <?php
-	require('views/article.phtml');
+	if ( isset($_GET['id']) ) {
+		$idArticle = $_GET['id'];
+		$query = "SELECT * FROM articles WHERE id = $idArticle";
+		$result = mysqli_query($database, $query);
+		$article = mysqli_fetch_assoc($result);
+		require('views/article.phtml');
+	}
 ?>
