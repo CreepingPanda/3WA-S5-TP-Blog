e85b9e9b241287747d850eeb54bef878df098e45
lucminarro <luc.minarro@gmail.com>
2015-11-17 14:52:51 +0100

    modifs

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	}
 
 	$ways = array('home', 'article', 'artlist', 'login', 'mp', 'newmp', 'newarticle', 'profil', 'register', 'userlist', 'archives', 'edit_profil');
-	$traitements = array('newarticle', 'login', 'mp', 'logout', 'addComment', 'newmp', 'register', 'note', 'edit_profil');
+	$traitements = array('newarticle', 'login', 'mp', 'logout', 'addComment', 'newmp', 'register', 'note', 'edit_profil', 'profil');
 
 	$traitementsAdmin = array();
 
