537854243c232de7f4b9724cde3a705eff35e7f8
CreepingPanda <dhrask@voila.fr>
2015-11-17 14:59:19 +0100

    Upload - Structure home

--- apps/home_last_article.php
+++ apps/home_last_article.php
@@ -1,3 +1,9 @@
 <?php
+
+	$lastArticleQuery = "SELECT * FROM articles ORDER BY id DESC LIMIT 0, 1";
+	$lastArticleResult = mysqli_query($database, $lastArticleQuery);
+	$lastArticle = mysqli_fetch_assoc($lastArticleResult);
+
 	require('views/home_last_article.phtml');
+
 ?>
--- apps/home_last_member.php
+++ apps/home_last_member.php
@@ -1,3 +1,9 @@
 <?php
+
+	$lastMemberQuery = "SELECT * FROM users ORDER BY id DESC LIMIT 0, 1";
+	$lastMemberResult = mysqli_query($database, $lastMemberQuery);
+	$lastMember = mysqli_fetch_assoc($lastMemberResult);
+
 	require('views/home_last_member.phtml');
+	
 ?>
