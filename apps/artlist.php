fdec2f82a35884f637fbe56785f794afcbc6b5c8
CreepingPanda <dhrask@voila.fr>
2015-11-17 11:24:32 +0100

    Update - Optimisation JOIN

--- apps/artlist.php
+++ apps/artlist.php
@@ -1,7 +1,7 @@
 <?php
 
 	$artlistQuery = "SELECT * FROM articles ORDER BY id DESC LIMIT 0, 5";
-	$authorQuery = "SELECT * FROM users INNER JOIN articles ON users.id = articles.id_author";
+	$authorQuery = "SELECT * FROM users LEFT JOIN articles ON users.id = articles.id_author";
 
 	$artlistResult = mysqli_query($database, $artlistQuery);
 	$authorResult = mysqli_query($database, $authorQuery);
