1f0b3c7e5f2aaf09df4d4014ff510404b187ff77
CreepingPanda <dhrask@voila.fr>
2015-11-17 15:20:29 +0100

    Update - home.php

--- apps/home_last_article.php
+++ apps/home_last_article.php
@@ -1,8 +1,14 @@
 <?php
 
-	$lastArticleQuery = "SELECT * FROM articles ORDER BY id DESC LIMIT 0, 1";
+	$lastArticleQuery = "SELECT * FROM articles WHERE validate = 1 ORDER BY id DESC LIMIT 0, 1";
 	$lastArticleResult = mysqli_query($database, $lastArticleQuery);
+	// ____ Dernier article validé ____
 	$lastArticle = mysqli_fetch_assoc($lastArticleResult);
+
+	$authorQuery = "SELECT * FROM users WHERE id = '".$lastArticle['id_author']."'";
+	$authorResult = mysqli_query($database, $authorQuery);
+	// ____ Auteur de l'article ____
+	$author = mysqli_fetch_assoc($authorResult);
 
 	require('views/home_last_article.phtml');
 
