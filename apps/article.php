acc0e283c53670ad467c7e63bc611d0606a5d7c0
CreepingPanda <dhrask@voila.fr>
2015-11-17 10:03:03 +0100

    Update - Display auteurs

--- apps/article.php
+++ apps/article.php
@@ -2,8 +2,13 @@
 	if ( isset($_GET['id']) ) {
 		$idArticle = $_GET['id'];
 		$select = "SELECT * FROM articles WHERE id = $idArticle";
+		$authorQuery = "SELECT * FROM users INNER JOIN articles ON users.id = articles.id_author";
+
 		$result = mysqli_query($database, $select);
+		$authorResult = mysqli_query($database, $authorQuery);
+
 		$article = mysqli_fetch_assoc($result);
+		$author = mysqli_fetch_assoc($authorResult);
 
 		require('views/article.phtml');
 	}
--- apps/artlist.php
+++ apps/artlist.php
@@ -1,7 +1,9 @@
 <?php
 
 	$artlistQuery = "SELECT * FROM articles LIMIT 5";
+	$authorQuery = "SELECT * FROM users INNER JOIN articles ON users.id = articles.id_author";
 	$artlistResult = mysqli_query($database, $artlistQuery);
+	$authorResult = mysqli_query($database, $authorQuery);
 	require('views/artlist.phtml');
 
 ?>
