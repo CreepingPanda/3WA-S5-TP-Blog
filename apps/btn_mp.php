38eada87d0f3013506ec930a4dc4cdb279c2b555
CreepingPanda <dhrask@voila.fr>
2015-11-18 16:47:30 +0100

    Update - Count unread

--- apps/btn_mp.php
+++ apps/btn_mp.php
@@ -1,11 +1,13 @@
 <?php
 
-if (isset($_SESSION['id']))
-{
-	
-		require('views/btn_mp.phtml');
+if ( isset($_SESSION['id']) && $_GET['id']==$_SESSION['id'] ) {
+	$unreadQuery = "SELECT COUNT(*) FROM p_messages WHERE read = 0 AND id_reader = '".$_SESSION['id']."'";
+	$unread = mysqli_query($database, $unreadQuery);
+}
 
-	
+if ( isset($_SESSION['id']) )
+{
+	require('views/btn_mp.phtml');
 }
 
 ?>
