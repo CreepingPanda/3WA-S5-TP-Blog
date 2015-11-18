a39b7d0d744965348f2f25c2e48f9dff2a5fb102
Hollux <you@example.com>
2015-11-18 14:40:42 +0100

    lala

--- apps/addMp.php
+++ apps/addMp.php
@@ -1,15 +1,14 @@
 <?php
 // $_GET['idTarget'] => exist => nombre entier
+if (isset($_GET['idTarget'])){
 $query = "SELECT login FROM users WHERE users.id=".intval($_GET['idTarget']);
 $idMp = mysqli_query($database,$query);
 $idMpTab = mysqli_fetch_assoc($idMp);
-if (isset($_GET['idTarget'])){
 	$loginTarget = $idMpTab['login'];
 }
 if (isset($_SESSION['id']))
 {
 	$content = '';
-	var_dump($idMpTab);
 	require('views/addMp.phtml');
 }
 ?>
