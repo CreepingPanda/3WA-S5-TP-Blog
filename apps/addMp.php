3282d78ca83045edee5d93d8230fac93ef20ab1d
Hollux <you@example.com>
2015-11-18 14:37:32 +0100

    tamere

--- apps/addMp.php
+++ apps/addMp.php
@@ -1,7 +1,15 @@
 <?php
+// $_GET['idTarget'] => exist => nombre entier
+$query = "SELECT login FROM users WHERE users.id=".intval($_GET['idTarget']);
+$idMp = mysqli_query($database,$query);
+$idMpTab = mysqli_fetch_assoc($idMp);
+if (isset($_GET['idTarget'])){
+	$loginTarget = $idMpTab['login'];
+}
 if (isset($_SESSION['id']))
 {
 	$content = '';
-		require('views/addMp.phtml');
+	var_dump($idMpTab);
+	require('views/addMp.phtml');
 }
 ?>
--- apps/comments.php
+++ apps/comments.php
@@ -1,6 +1,6 @@
 <?php
 // apps/articles.php
-$query = "SELECT * FROM `comments` LEFT JOIN users ON users.id=comments.id_user WHERE id_article='".$article['id']."' LIMIT 0, 30 ";
+$query = "SELECT * FROM comments LEFT JOIN users ON users.id=comments.id_user WHERE id_article='".$article['id']."' LIMIT 0, 30 ";
 $resultatComment = mysqli_query($database, $query);
 	while($comment = mysqli_fetch_assoc($resultatComment))
 	{
