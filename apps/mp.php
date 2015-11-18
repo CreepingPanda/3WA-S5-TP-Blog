121832b851f835f7b74e431197fe25d56a98e3a5
Hollux <you@example.com>
2015-11-18 15:52:13 +0100

    tamere

--- apps/mp.php
+++ apps/mp.php
@@ -1,5 +1,5 @@
 <?php 
-	$query = "SELECT * FROM p_messages LEFT JOIN users ON users.id=p_messages.id_writer WHERE id_reader='".$_SESSION['id']."'";  
+	$query = "SELECT * FROM p_messages LEFT JOIN users ON users.id=p_messages.id_writer WHERE id_reader='".$_SESSION['id']."' ORDER BY p_messages.date DESC";
 	$mpPerso = mysqli_query($database, $query);
 	while($mp = mysqli_fetch_assoc($mpPerso))
 	{
