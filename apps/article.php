03131db1e89d5bf5dc9941a9dd2e5ddc86817acf
CreepingPanda <dhrask@voila.fr>
2015-11-18 14:25:55 +0100

    Update - Catégories

--- apps/article.php
+++ apps/article.php
@@ -4,12 +4,15 @@
 
 		$articleQuery = "SELECT * FROM articles WHERE id = $idArticle";
 		$authorQuery = "SELECT * FROM users INNER JOIN articles ON users.id = articles.id_author";
+		$categoryQuery = "SELECT * FROM category LEFT JOIN articles ON category.id = articles.id_category";
 		
 		$articleResult = mysqli_query($database, $articleQuery);
 		$authorResult = mysqli_query($database, $authorQuery);
+		$categoryResult = mysqli_query($database, $categoryQuery);
 
 		$article = mysqli_fetch_assoc($articleResult);
 		$author = mysqli_fetch_assoc($authorResult);
+		$category = mysqli_fetch_assoc($categoryResult);
 
 		require('views/article.phtml');
 	}
