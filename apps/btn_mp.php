a617733264f15ce75554b371136a4b0bcc8d3f46
CreepingPanda <dhrask@voila.fr>
2015-11-19 09:17:31 +0100

    Update - Sécurité get

--- apps/btn_mp.php
+++ apps/btn_mp.php
@@ -3,9 +3,8 @@
 	if ( isset($_SESSION['id']) && $_GET['id']==$_SESSION['id'] ) {
 		$unreadQuery = "SELECT COUNT(*) AS count FROM p_messages WHERE read = 0 AND id_reader = '".$_SESSION['id']."'";
 		$unreadResult = mysqli_query($database, $unreadQuery);
-		echo mysqli_error($database);
-		$unread = mysqli_fetch_assoc($unreadResult);
-		$unread = $unread['count'];
+		$unreadAssoc = mysqli_fetch_assoc($unreadResult);
+		$unread = $unreadAssoc['count'];
 	}
 
 	if ( isset($_SESSION['id']) )
--- apps/traitement_article.php
+++ apps/traitement_article.php
@@ -3,7 +3,7 @@
 	{
 		if ( isset($_POST['delete']) && $_POST['delete']=="SUPPRIMER")
 		{
-			$deleteQuery = "DELETE FROM articles WHERE id = '".$_GET['id']."'";
+			$deleteQuery = "DELETE FROM articles WHERE id = '".intval($_GET['id'])."'";
 			mysqli_query($database, $deleteQuery);
 
 			header('Location:index.php?page=artlist');
