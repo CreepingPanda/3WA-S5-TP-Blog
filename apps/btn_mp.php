095e70660a4bc49e7849ad22366bff6d16566aea
CreepingPanda <dhrask@voila.fr>
2015-11-19 10:03:38 +0100

    Update - Unread navlog

--- apps/btn_mp.php
+++ apps/btn_mp.php
@@ -1,14 +1,5 @@
 <?php
 
-	if ( isset($_SESSION['id']) && $_GET['id']==$_SESSION['id'] )
-	{
-		$reader = intval($_SESSION['id']);
-		$unreadQuery = "SELECT COUNT(*) AS count FROM p_messages WHERE `read` = 0 AND id_reader = $reader";
-		$unreadResult = mysqli_query($database, $unreadQuery);
-		$unreadAssoc = mysqli_fetch_assoc($unreadResult);
-		$unread = $unreadAssoc['count'];
-	}
-
 	if ( isset($_SESSION['id']) )
 	{
 		require('views/btn_mp.phtml');
