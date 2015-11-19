0e4299d9fc8494a70305936293c47b065947c22b
lambertmeyer <meyer.lambert68@gmail.com>
2015-11-19 09:42:53 +0100

    kk

--- apps/traitement_statut.php
+++ apps/traitement_statut.php
@@ -1,15 +1,19 @@
 <?php
-// echo "toto";
-// var_dump($_POST, $_GET); 
+echo "toto";
+var_dump($_POST, $_GET); 
+
+$id = $_POST['id'];
+$statut = $_POST['statut'];
 
 
 
-$statut="UPDATE users SET rights WHERE rights=statut" ;
+
+$statut="UPDATE users SET rights='".$statut."' WHERE id=".$id ;
 
 
 mysqli_query($database, $statut);
 
-
+header('Location:index.php?page=userlist ');
 exit;
 
 
