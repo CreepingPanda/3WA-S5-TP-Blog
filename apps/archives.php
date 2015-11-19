82a5ea41468e409981cfa8980419fa4873769e34
CreepingPanda <dhrask@voila.fr>
2015-11-19 13:23:41 +0100

    Update - Fix auteurs

--- apps/archives.php
+++ apps/archives.php
@@ -3,9 +3,6 @@
 	$archivesQuery = "SELECT * FROM articles WHERE validate = true ORDER BY id DESC";
 	$archivesResult = mysqli_query($database, $archivesQuery);
 
-	$authorQuery = "SELECT * FROM users LEFT JOIN articles ON users.id = articles.id_author";
-	$authorResult = mysqli_query($database, $authorQuery);
-
 	require('views/archives.phtml');
 
 ?>
