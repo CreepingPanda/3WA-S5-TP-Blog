81502be9d12e89067e34a2772b7849ead1527dae
lucminarro <luc.minarro@gmail.com>
2015-11-19 11:39:59 +0100

    correction

--- apps/traitement_note.php
+++ apps/traitement_note.php
@@ -1,20 +1,23 @@
 <?php
-$content = "";
-if (isset($_POST['value'], $_POST['idarticle']))
+
+if (isset($_POST['value'], $_POST['idarticle'], $_SESSION['id']))
 {
+	$idArticle = $_POST['idarticle'];
+	$idSession = $_SESSION['id'];
 	$value = mysqli_real_escape_string($database, $_POST['value']);
+
 	if (($_POST['value']) < 0 || ($_POST['value']) > 5)
 	{
 		$errors[] = "Incorrect value, must be between 0 and 5";
 	}
 	if (count($errors) == 0)
 	{
-		$query = "INSERT INTO notes (id_article, id_user, value) VALUES('".$_POST['idarticle']."', '".$_SESSION['id']."', '".$value."')";
-		$resultat = mysqli_query($database, $query);
+		$query = "INSERT INTO notes (id_article, id_user, value) VALUES('$idArticle', '$idSession', '$value')";
+		mysqli_query($database, $query);
 
 		if ($resultat)
 		{
-			header('Location: ?page=article&id='.$_POST['idarticle'].''); 
+			header("Location: ?page=article&id='$idArticle'"); 
 			exit;
 		}
 		else
