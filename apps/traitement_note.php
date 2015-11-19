321d227f733276a23b330d8f225df899750b236a
lucminarro <luc.minarro@gmail.com>
2015-11-19 12:54:00 +0100

    ajout du systeme de notation des articles

--- apps/traitement_note.php
+++ apps/traitement_note.php
@@ -1,23 +1,21 @@
 <?php
-
 if (isset($_POST['value'], $_POST['idarticle'], $_SESSION['id']))
 {
 	$idArticle = $_POST['idarticle'];
 	$idSession = $_SESSION['id'];
-	$value = mysqli_real_escape_string($database, $_POST['value']);
+	$value = intval($_POST['value']);
 
-	if (($_POST['value']) < 0 || ($_POST['value']) > 5)
+	if ($value < 0 || $value > 5)
 	{
 		$errors[] = "Incorrect value, must be between 0 and 5";
 	}
 	if (count($errors) == 0)
 	{
-		$query = "INSERT INTO notes (id_article, id_user, value) VALUES('$idArticle', '$idSession', '$value')";
-		mysqli_query($database, $query);
-
+		$query = "INSERT INTO notes (id_article, id_user, value) VALUES('".$idArticle."', '".$idSession."', '".$value."')";
+		$resultat = mysqli_query($database, $query);
 		if ($resultat)
 		{
-			header("Location: ?page=article&id='$idArticle'"); 
+			header("Location: ?page=article&id='.$idArticle.'"); 
 			exit;
 		}
 		else
