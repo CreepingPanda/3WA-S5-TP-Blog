f6af9225a5fc1996b96ef3ad123a4e7698c10562
lucminarro <luc.minarro@gmail.com>
2015-11-20 13:29:16 +0100

    modifs home

--- apps/home_last_member.php
+++ apps/home_last_member.php
@@ -1,10 +1,11 @@
 <?php
 
-	$lastMemberQuery = "SELECT * FROM users ORDER BY id DESC LIMIT 0, 1";
+	$lastMemberQuery = "SELECT * FROM users ORDER BY id DESC LIMIT 0, 6";
 	$lastMemberResult = mysqli_query($database, $lastMemberQuery);
-	// ____ Dernier membre inscrit ____
-	$lastMember = mysqli_fetch_assoc($lastMemberResult);
+	
 
+while ( $lastMember = mysqli_fetch_assoc($lastMemberResult) ) {
 	require('views/home_last_member.phtml');
+}
 
 ?>
